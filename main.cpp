db3c6ab8bc8cd20b995fb8cc36e4e1e664e4d48e
一希 伊原 <k023g0076@g.neec.ac.jp>
2024-04-12 10:13:48 +0900

    NoviceResources

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 ﻿#include <Novice.h>
 #include "MyFunctions.h"
 
-const char kWindowTitle[] = "学籍番号";
+const char kWindowTitle[] = "MT3";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
