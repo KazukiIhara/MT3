f5d45cb76f168bde459eda4e80db6bfe2262ec4f
IharaKazuki <kazuto3996@gmail.com>
2024-04-18 19:57:55 +0900

    00_03無事完了

--- MyFunctions.h
+++ MyFunctions.h
@@ -69,3 +69,7 @@ Matrix4x4 Inverse(const Matrix4x4& a);
 Matrix4x4 Transpose(const Matrix4x4& a);
 Matrix4x4 MakeIdentity4x4();
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
+
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+Vector3 Transform(const  Vector3& vector, const  Matrix4x4& matrix);
--- main.cpp
+++ main.cpp
@@ -13,7 +13,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
-	
+	Vector3 translate{ 4.1f,2.6f,0.8f };
+	Vector3 scale{ 1.5f,5.2f,7.3f };
+	Matrix4x4 translateMatrix = MakeTranslateMatrix(translate);
+	Matrix4x4 scaleMatrix = MakeScaleMatrix(scale);
+	Vector3 point{ 2.3f,3.8f,1.4f };
+	Matrix4x4 transformMatrix = {
+		1.0f,2.0f,3.0f,4.0f,
+		3.0f,1.0f,1.0f,2.0f,
+		1.0f,4.0f,2.0f,3.0f,
+		2.0f,2.0f,1.0f,3.0f,
+	};
+	Vector3 transformed = Transform(point, transformMatrix);
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0)
@@ -38,7 +49,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		///
 		/// ↓描画処理ここから
 		///
-
+		VectorScreenPrintf(0, 0, transformed, "transformed");
+		MatrixScreenPrintf(0, 24, translateMatrix, "translateMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5 + 24, scaleMatrix, "scaleMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
