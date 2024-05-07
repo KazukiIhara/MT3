5be29bc48e4255628e5682b0174771890b88e372
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-05-07 19:08:34 +0900

    01_01無事終了

--- main.cpp
+++ main.cpp
@@ -18,6 +18,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	Vector3 cross = Cross(v1, v2);
 	VectorScreenPrintf(0, 0, cross, "Cross");
 
+	Vector3 rotate{ 0.0f,0.0f,0.0f };
+	Vector3 translate{ 0.0f,0.0f,1.0f };
+	Vector3 kLocalVertex[3];
+	kLocalVertex[0] = { -1.0f,1.0f,0.0f };
+	kLocalVertex[1] = { 0.0f,-1.0f,0.0f };
+	kLocalVertex[2] = { 1.0f,1.0f,0.0f };
+
+	Vector3 cameraPosition{ 0.0f,0.0f,10.0f };
+
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0)
 	{
@@ -32,6 +42,36 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		///
 		/// ↓更新処理ここから
 		///
+		rotate.y -= 0.03f;
+		if (keys[DIK_W])
+		{
+			translate.z -= 0.1f;
+		}
+		if (keys[DIK_S])
+		{
+			translate.z += 0.1f;
+		}
+		if (keys[DIK_A])
+		{
+			translate.x += 0.1f;
+		}
+		if (keys[DIK_D])
+		{
+			translate.x -= 0.1f;
+		}
+
+		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
+		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, cameraPosition);
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kScreenWidth) / float(kScreenHeight), 0.1f, 100.0f);
+		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kScreenWidth), float(kScreenHeight), 0.0f, 1.0f);
+		Vector3 screenVertices[3];
+		for (uint32_t i = 0; i < 3; i++)
+		{
+			Vector3 ndcVertex = Transform(kLocalVertex[i], worldViewProjectionMatrix);
+			screenVertices[i] = Transform(ndcVertex, viewportMatrix);
+		}
 
 
 		///
@@ -43,6 +83,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		///
 
 		VectorScreenPrintf(0, 0, cross, "Cross");
+
+		Novice::DrawTriangle(
+			int(screenVertices[0].x), int(screenVertices[0].y),
+			int(screenVertices[1].x), int(screenVertices[1].y),
+			int(screenVertices[2].x), int(screenVertices[2].y),
+			RED, kFillModeSolid
+		);
 
 		///
 		/// ↑描画処理ここまで
