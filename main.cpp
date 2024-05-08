8c370aae97ed0807719c71ed38ba4a08a83451d9
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-05-08 13:18:41 +0900

    グリッドの描画まで終了

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 ﻿#include <Novice.h>
 #include "MyFunctions.h"
+#include "imgui.h"
+
 
 const char kWindowTitle[] = "LE2B_02_イハラ_カズキ_MT3";
 void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix)
@@ -7,17 +9,44 @@ void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMa
 	const float kGridHalfWidth = 2.0f;
 	const uint32_t kSubdivision = 10;
 	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision);
+	Vector3 kLocalGrid{ 0.0f,0.0f,0.0f };
+
 	//奥から手前への線を隅々にひいていく
 	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex)
 	{
-		/*float startX = -kGridHalfWidth + xIndex * kGridEvery;
-		float startY = -kGridHalfWidth;
-		float endX = startX;
-		float endY = kGridHalfWidth;*/
-		Vector3 startPos = { -kGridHalfWidth + xIndex * kGridEvery,-kGridHalfWidth,0.0f };
-		Vector3 endPos = { -kGridHalfWidth + xIndex * kGridEvery,kGridHalfWidth };
-		Novice::DrawLine(int(startPos.x), int(startPos.y), int(endPos.x), int(endPos.y), 0xaaaaaaff);
+		Vector3 startPos = { -kGridHalfWidth + xIndex * kGridEvery,0.0f,-kGridHalfWidth };
+		Vector3 endPos = { -kGridHalfWidth + xIndex * kGridEvery,0.0f,kGridHalfWidth };
+
+		Matrix4x4 sWorldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f, 0.0f, 0.0f }, startPos);
+		Matrix4x4 sWorldViewProjectionMatrix = Multiply(sWorldMatrix, viewProjectionMatrix);
+		Vector3 ndcStart = Transform(kLocalGrid, sWorldViewProjectionMatrix);
+		Vector3 screenStart = Transform(ndcStart, viewportMatrix);
+
+		Matrix4x4 eWorldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f, 0.0f, 0.0f }, endPos);
+		Matrix4x4 eWorldViewProjectionMatrix = Multiply(eWorldMatrix, viewProjectionMatrix);
+		Vector3 ndcEnd = Transform(kLocalGrid, eWorldViewProjectionMatrix);
+		Vector3 screenEnd = Transform(ndcEnd, viewportMatrix);
+
+		Novice::DrawLine(int(screenStart.x), int(screenStart.y), int(screenEnd.x), int(screenEnd.y), 0xaaaaaaff);
 	}
+	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex)
+	{
+		Vector3 startPos = { -kGridHalfWidth,0.0f,-kGridHalfWidth + xIndex * kGridEvery };
+		Vector3 endPos = { kGridHalfWidth,0.0f,-kGridHalfWidth + xIndex * kGridEvery };
+
+		Matrix4x4 sWorldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f, 0.0f, 0.0f }, startPos);
+		Matrix4x4 sWorldViewProjectionMatrix = Multiply(sWorldMatrix, viewProjectionMatrix);
+		Vector3 ndcStart = Transform(kLocalGrid, sWorldViewProjectionMatrix);
+		Vector3 screenStart = Transform(ndcStart, viewportMatrix);
+
+		Matrix4x4 eWorldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f, 0.0f, 0.0f }, endPos);
+		Matrix4x4 eWorldViewProjectionMatrix = Multiply(eWorldMatrix, viewProjectionMatrix);
+		Vector3 ndcEnd = Transform(kLocalGrid, eWorldViewProjectionMatrix);
+		Vector3 screenEnd = Transform(ndcEnd, viewportMatrix);
+
+		Novice::DrawLine(int(screenStart.x), int(screenStart.y), int(screenEnd.x), int(screenEnd.y), 0xaaaaaaff);
+	}
+
 }
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -30,10 +59,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
 
-
-
-	Vector3 cameraPosition{ 0.0f,0.0f,0.0f };
-
+	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };
+	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0)
@@ -46,10 +73,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 
 		Novice::SetWindowMode(windowMode);
 
+		//Imgui
+		ImGui::Begin("");
+
+
+
 		///
 		/// ↓更新処理ここから
 		///
-
+		Matrix4x4 cameraMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kScreenWidth) / float(kScreenHeight), 0.1f, 100.0f);
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kScreenWidth), float(kScreenHeight), 0.0f, 1.0f);
+		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
 
 
 		///
@@ -59,7 +95,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		///
 		/// ↓描画処理ここから
 		///
-		DrawGrid();
+		DrawGrid(viewProjectionMatrix, viewportMatrix);
 		///
 		/// ↑描画処理ここまで
 		///
