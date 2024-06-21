5c2f63eb1bc86aed0d47f00b4eab69bf2461dba7
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-06-21 11:45:00 +0900

    maincppをリセット

--- main.cpp
+++ main.cpp
@@ -17,21 +17,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	Vector3 cameraTranslate{ 0.00f,1.90f,-5.0f };
 	Vector3 cameraRotate{ 0.39f,0.0f,0.0f };
 
-	Vector3 rotate{ 0.0f,0.0f,0.0f };
-	OBB obb{
-		.center{-1.0f,0.0f,0.0f},
-		.orientations = {{1.0f,0.0f,0.0f},
-						 {0.0f,1.0f,0.0f},
-						 {0.0f,0.0f,1.0f}},
-		.size{ 0.5f,0.5f,0.5f }
-	};
-
-	Sphere sphere{
-		.center{0.0f,0.0f,0.0f},
-		.rotate{0.0f,0.0f,0.0f},
-		.radius{0.5f},
-		.color = WHITE,
-	};
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0)
@@ -49,10 +34,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		ImGui::DragFloat3("cameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("cameraRotate", &cameraRotate.x, 0.01f);
 
-		ImGui::DragFloat3("obbTranslate", &obb.center.x, 0.01f);
-		ImGui::DragFloat3("obbSize", &obb.size.x, 0.01f);
-		ImGui::DragFloat3("obbRotate", &rotate.x,0.01f);
-
 
 		ImGui::End();
 
@@ -65,30 +46,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kScreenWidth), float(kScreenHeight), 0.0f, 1.0f);
 		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
 
-		Matrix4x4 rotateMatrix = MakeRotateXYZMatrix(rotate);
-
-		obb.orientations[0].x = rotateMatrix.m[0][0];
-		obb.orientations[0].y = rotateMatrix.m[0][1];
-		obb.orientations[0].z = rotateMatrix.m[0][2];
-
-		obb.orientations[1].x = rotateMatrix.m[1][0];
-		obb.orientations[1].y = rotateMatrix.m[1][1];
-		obb.orientations[1].z = rotateMatrix.m[1][2];
-
-		obb.orientations[2].x = rotateMatrix.m[2][0];
-		obb.orientations[2].y = rotateMatrix.m[2][1];
-		obb.orientations[2].z = rotateMatrix.m[2][2];
-
-
-		if (IsCollision(obb, sphere))
-		{
-			sphere.color = RED;
-		}
-		else
-		{
-			sphere.color = WHITE;
-		}
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -98,9 +55,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		///
 		DrawGrid(viewProjectionMatrix, viewportMatrix);
 
-		DrawOBB(obb, viewProjectionMatrix, viewportMatrix, WHITE);
-
-		DrawSphere(sphere, viewProjectionMatrix, viewportMatrix);
 		///
 		/// ↑描画処理ここまで
 		///
