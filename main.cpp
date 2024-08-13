3a14014f590b8805b86564daf824a6a8976aedd0
KazukiIhara <k023g0076@g.neec.ac.jp>
2024-08-13 11:52:43 +0900

    main.cppのクリーン

--- main.cpp
+++ main.cpp
@@ -16,23 +16,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Vector3 cameraTranslate{ 0.00f,1.90f,-5.0f };
 	Vector3 cameraRotate{ 0.39f,0.0f,0.0f };
 
-	float deltaTime = 1.0f / 60.0f;
-
-	bool isUpdate = false;
-
-	Plane plane{};
-	plane.normal = Normalize(Vector3(-0.2f, 0.9f, -0.3f));
-	plane.distance = 0.0f;
-
-	Ball ball{};
-	ball.position = { 0.8f,1.2f,0.3f };
-	ball.mass = 2.0f;
-	ball.radius = 0.05f;
-	ball.acceleration = { 0.0f,-9.8f,0.0f };
-	ball.velocity = { 0.0f,0.0f,0.0f };
-	ball.color = WHITE;
-
-	float e = 0.3f;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -45,13 +28,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Novice::SetWindowMode(windowMode);
 
 		//Imgui
-		ImGui::Begin("Window");
+		ImGui::Begin("Camera");
 		ImGui::DragFloat3("cameraTranslate", &cameraTranslate.x, 0.01f);
 		ImGui::DragFloat3("cameraRotate", &cameraRotate.x, 0.01f);
-
-		if (ImGui::Button("Start")) {
-			isUpdate = true;
-		}
 		ImGui::End();
 
 		///
@@ -63,21 +42,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kScreenWidth), float(kScreenHeight), 0.0f, 1.0f);
 		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
 
-		if (isUpdate) {
-			ball.velocity += ball.acceleration * deltaTime;
-			ball.position += ball.velocity * deltaTime;
-		}
-		Sphere sphere = { ball.position,{0.0f,0.0f,0.0f},ball.radius };
-		if (IsCollision(sphere, plane)) {
-			Vector3 reflected = Reflect(ball.velocity, plane.normal);
-			Vector3 projectToNormal = Project(reflected, plane.normal);
-			Vector3 movingDirection = reflected - projectToNormal;
-			ball.velocity = projectToNormal * e + movingDirection;
-		}
-
-		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, ball.position);
-
-
 		///
 		/// ↑更新処理ここまで
 		///
@@ -87,8 +51,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 
 		DrawGrid(viewProjectionMatrix, viewportMatrix);
-		DrawPlane(plane, viewProjectionMatrix, viewportMatrix, WHITE);
-		DrawSphereWorldMatrix(worldMatrix, viewProjectionMatrix, viewportMatrix, WHITE);
 
 		///
 		/// ↑描画処理ここまで
